b3d96c4779374eb6cc804ed50c58568a3cef6504
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-04 20:51:42 +0100

    SvelteKit with DaisyUI/Theme Select

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.postcss';
+	let theme = 'cupcake';
+</script>
+
+<div data-theme={theme} class="min-h-screen">
+	<select bind:value={theme} class="fixed right-0">
+		<option value="cupcake">Cupcake</option>
+		<option value="synthwave">Synthwave</option>
+		<option value="cyberpunk">Cyberpunk</option>
+	</select>
+	<slot />
+</div>

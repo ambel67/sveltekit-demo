6a5b9410302c444d0d3b5de2f144808708dd5073
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-12-19 11:10:20 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,39 @@
 	import Icon from '@iconify/svelte';
 	import { onMount } from 'svelte';
 	let countdown = 60;
+	let cd = {
+		seconds: 8,
+		minutes: 10,
+		hours: 3,
+		days: 0
+	};
+
+	function count() {
+		if (cd.seconds > 0) {
+			cd.seconds--;
+		} else {
+			cd.seconds = 59;
+			if (cd.minutes > 0) {
+				cd.minutes--;
+			} else {
+				cd.minutes = 59;
+				if (cd.hours > 0) {
+					cd.hours--;
+				} else {
+					cd.hours = 23;
+					if (cd.days > 0) {
+						cd.days--;
+					} else {
+						cd.days = 0;
+					}
+				}
+			}
+		}
+	}
 	onMount(() => {
 		setInterval(() => (countdown = countdown - 1), 1000);
 		setInterval(() => (stock = stock - 1), Math.random() * 10000 + 5000);
+		setInterval(count, 1000);
 	});
 	let stock = 10;
 </script>
@@ -17,3 +47,30 @@
 	<span style="--value:{countdown};" />
 </span>
 <div class="radial-progress text-primary" style="--value:{stock};">{stock}</div>
+
+<div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+	<div class="flex flex-col">
+		<span class="countdown font-mono text-5xl">
+			<span style="--value:{cd.days};" />
+		</span>
+		days
+	</div>
+	<div class="flex flex-col">
+		<span class="countdown font-mono text-5xl">
+			<span style="--value:{cd.hours};" />
+		</span>
+		hours
+	</div>
+	<div class="flex flex-col">
+		<span class="countdown font-mono text-5xl">
+			<span style="--value:{cd.minutes};" />
+		</span>
+		min
+	</div>
+	<div class="flex flex-col">
+		<span class="countdown font-mono text-5xl">
+			<span style="--value:{cd.seconds};" />
+		</span>
+		sec
+	</div>
+</div>

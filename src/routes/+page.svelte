3ed2bfa2fd69f43dfed5f0c1d5ab590e6c97358a
ambel67 <84065215+ambel67@users.noreply.github.com>
2024-02-27 13:53:44 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
-<h1 class="text-blue-900 bg-slate-400 text-5xl">home</h1>
-<p class="bg-green-300 text-green-800">this is the home page.</p>
-<button class="btn text-pink-950 bg-slate-400">Hey</button>
+<script>
+	import { supabase } from '$lib/supabase';
+
+	let promise = supabase.from('countries').select('*');
+</script>
+
+{#await promise}
+	<div>...loading</div>
+{:then result}
+	<div>{JSON.stringify(result)}</div>
+{/await}
